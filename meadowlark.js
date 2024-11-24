7c15cf6dabf7224bef09e50730fd68cace62a0a5
hagansamuel6 <hagansamuel6@gmail.com>
2024-11-24 14:57:30 +0000

    public static folder

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,7 @@ const engine = require("express-handlebars");
 
 const app = express();
 
+//handlebars as default engine setup
 app.engine(
   "handlebars",
   engine.engine({
@@ -15,18 +16,34 @@ app.set("views", "./views");
 
 const port = process.env.PORT || 3000;
 
+//set the static folder middleware
+app.use(express.static(__dirname + "/public"))
+
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+  ]
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render("about", {fortune: randomFortune});
 });
 
 app.get("/test", (req, res) => {
   res.type("text/plain");
   res.send("testing");
 });
+
+app.get("/u", (req, res) => {
+  res.send("working");
+})
 
 app.use((req, res) => {
   res.status(404);
